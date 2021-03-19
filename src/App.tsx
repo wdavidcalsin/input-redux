2b62acb517113b9f8ac8b740cbc1becee2646039
WDavid Calsin <wdavidcalsin968@gmail.con>
2021-03-19 13:21:57 -0500

    feat: 🎸 remove function

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import { addAction } from "./actions/add.action";
+import { addAction, deleteAction } from "./actions/add.action";
 import { v4 as uuidv4 } from "uuid";
 
 interface IValue {
@@ -35,13 +35,25 @@ function App() {
     <div className="App">
       <h3>Todo List</h3>
       <form action="" onSubmit={handleSubmit}>
-        <input type="text" onChange={handleChange} value={valueInput.value} />
+        <input
+          type="text"
+          onChange={handleChange}
+          value={valueInput.value || ""}
+        />
         <button type="submit">save</button>
       </form>
       <ul className="list">
         {addValue.map((index: any, key: any) => (
           <li key={key}>
             {key}: {index.value}
+            <button
+              type="button"
+              onClick={() => dispatch(deleteAction(index.id))}
+            >
+              Delete
+            </button>
+            <br />
+            <br />
           </li>
         ))}
       </ul>
